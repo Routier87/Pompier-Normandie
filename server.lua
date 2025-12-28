be5c35d9235f36e970be5749165c931944ee9578
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2025-12-29 00:00:55 +0100

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,35 @@
+ESX = exports["es_extended"]:getSharedObject()
+
+RegisterServerEvent('pompier:annonce')
+AddEventHandler('pompier:annonce', function(type)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer.job.name ~= 'pompier' then return end
+
+    local data = {
+        open = {
+            title = "Pompier Normandie",
+            subtitle = "🟢 EN SERVICE",
+            msg = "Les Pompiers de Normandie sont désormais en service."
+        },
+        close = {
+            title = "Pompier Normandie",
+            subtitle = "🔴 HORS SERVICE",
+            msg = "Les Pompiers de Normandie sont hors service."
+        },
+        inter = {
+            title = "Pompier Normandie",
+            subtitle = "🚨 INTERVENTION",
+            msg = "Intervention en cours, merci d'éviter la zone."
+        },
+        fire = {
+            title = "Pompier Normandie",
+            subtitle = "🔥 INCENDIE",
+            msg = "Incendie majeur en cours."
+        }
+    }
+
+    local d = data[type]
+    if not d then return end
+
+    TriggerClientEvent('pompier:showAnnonce', -1, d)
+end)
