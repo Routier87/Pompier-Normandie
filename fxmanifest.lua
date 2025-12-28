ba2ea1d6722a89bddea66c68f76f380abc19d1b8
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2025-12-29 00:00:30 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'routier87'
+description 'Pompier Normandie'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/sp_logo.png'
+}
+
+lua54 'yes'
